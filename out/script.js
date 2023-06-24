0fa2dd3385baa3ed0cb6759d7558f02c3d1ed304
batoripeter <batorifoto@gmail.com>
2023-06-24 13:10:36 +0200

    basics

---
+++ out/script.js
@@ -0,0 +1,5 @@
+"use strict";
+let inputField = document.createElement("INPUT");
+inputField.setAttribute("type", "text");
+inputField.setAttribute("value", "City");
+document.body.appendChild(inputField);
---
+++ script.ts
@@ -0,0 +1,5 @@
+
+    let inputField = document.createElement("INPUT");
+    inputField.setAttribute("type", "text");
+    inputField.setAttribute("value", "City");
+    document.body.appendChild(inputField);
