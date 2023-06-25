5049e785319b19ea299e2d6443b076e165b45389
batoripeter <batorifoto@gmail.com>
2023-06-25 10:08:09 +0200

    city selector done

--- out/script.js
+++ out/script.js
@@ -8,21 +8,67 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+// create input into div
+let inputField = document.createElement("input");
+inputField.setAttribute('type', 'text');
+inputField.setAttribute('id', 'inputField');
+inputField.setAttribute('list', 'citieslist');
+inputField.setAttribute('value', '');
+inputField.className = "css-class-name";
+document.getElementById("root").appendChild(inputField);
+//create p into div for output
+let textField = document.createElement("p");
+textField.setAttribute('id', 'result-header');
+document.getElementById("root").appendChild(textField);
+//create selectable list  into div for input
+let cityDatalist = document.createElement("datalist");
+cityDatalist.setAttribute('id', 'citieslist');
+document.getElementById("root").appendChild(cityDatalist);
+//fetch if input >3 
 let myEventListener = (event) => __awaiter(void 0, void 0, void 0, function* () {
     let userInput = event.target.value;
-    let response = yield fetch("http://api.weatherapi.com/v1/current.json?q=" + userInput + "&key=6885d9accf8948e6acc113000232406");
-    let data = yield response.json();
-    render(data);
+    if (userInput.length > 3) {
+        let response = yield fetch("http://api.weatherapi.com/v1/current.json?q=" +
+            userInput +
+            "&key=6885d9accf8948e6acc113000232406");
+        let data = yield response.json();
+        render(data);
+    }
 });
 // input
 let resultHeaderElement = document.getElementById("result-header");
-let inputElement = document.getElementById("root");
+let inputElement = document.getElementById("inputField");
 inputElement.addEventListener("input", myEventListener);
 //output
 let render = (content) => {
-    resultHeaderElement.innerHTML = "<h1>" + "Temperature: " + content["current"]["temp_c"] + " Celsius"
-        + "<br>" +
-        "Temperature: " + content["current"]["temp_f"] + " Fahrenheit" + "<br>";
-    "</h1>";
-    resultHeaderElement.className = "aClassName";
+    resultHeaderElement.innerHTML =
+        "<h1>" +
+            content["location"]["name"] +
+            "<br>" +
+            "Temperature: " +
+            content["current"]["temp_c"] +
+            " Celsius" +
+            "<br>" +
+            "</h1>";
 };
+resultHeaderElement.className = "aClassName";
+//cities list loading 
+let options = "";
+function foo() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let obj;
+        const res = yield fetch("./citylist.json");
+        obj = yield res.json();
+        let i = 0;
+        let cityNames = [];
+        while (obj["cities"][i] !== undefined) {
+            cityNames = [...cityNames, obj["cities"][i]["name"]];
+            options += '<option value="' + cityNames[i] + '" />';
+            i++;
+        }
+        /*console.log(cityNames);*/
+        document.getElementById("citieslist").innerHTML = options;
+    });
+}
+foo();
+/* cities end */
--- script.ts
+++ script.ts
@@ -1,21 +1,75 @@
 
-let myEventListener =  async (event: Event) => {
-  let userInput = (event.target as HTMLInputElement).value
-  let response = await fetch ("http://api.weatherapi.com/v1/current.json?q="+userInput+"&key=6885d9accf8948e6acc113000232406")
-let data = await response.json()
-render(data)
-}
+// create input into div
+let inputField = document.createElement("input");
+inputField.setAttribute('type', 'text');
+inputField.setAttribute('id', 'inputField');
+inputField.setAttribute('list', 'citieslist');
+inputField.setAttribute('value', '');
+inputField.className = "css-class-name"; 
+document.getElementById("root")!.appendChild(inputField );
+
+//create p into div for output
+let textField = document.createElement("p");
+textField.setAttribute('id', 'result-header');
+document.getElementById("root")!.appendChild(textField);
+
+//create selectable list  into div for input
+let cityDatalist = document.createElement("datalist");
+cityDatalist.setAttribute('id', 'citieslist');
+document.getElementById("root")!.appendChild(cityDatalist);
+
+
+
+//fetch if input >3 
+let myEventListener = async (event: Event) => {
+  let userInput = (event.target as HTMLInputElement).value;
+  if (userInput.length>3) {
+  let response = await fetch(
+    "http://api.weatherapi.com/v1/current.json?q=" +
+      userInput +
+      "&key=6885d9accf8948e6acc113000232406"
+  )
+  let data = await response.json();
+  render(data);
+  }
+};
 
 // input
-let resultHeaderElement = document.getElementById("result-header")!
-let inputElement = document.getElementById("root")!
-inputElement.addEventListener("input", myEventListener)
+let resultHeaderElement = document.getElementById("result-header")!;
+let inputElement = document.getElementById("inputField")!;
+inputElement.addEventListener("input", myEventListener);
 
 //output
 let render = (content: any) => {
-    resultHeaderElement.innerHTML = "<h1>"+"Temperature: " + content["current"]["temp_c"] + " Celsius"
-    +"<br>"+
-    "Temperature: " + content["current"]["temp_f"] + " Fahrenheit"+"<br>"+"</h1>"
-    resultHeaderElement.className = "aClassName"
+  resultHeaderElement.innerHTML =
+    "<h1>" +
+    content["location"]["name"] +
+    "<br>" +
+    "Temperature: " +
+    content["current"]["temp_c"] +
+    " Celsius" +
+    "<br>" +
+    "</h1>";
+};
+resultHeaderElement.className = "aClassName";
 
+//cities list loading 
+let options = "";
+
+async function foo() {
+  let obj;
+  const res = await fetch("./citylist.json");
+  obj = await res.json();
+  let i = 0;
+  let cityNames: string[] = [];
+  while (obj["cities"][i] !== undefined) {
+    cityNames = [...cityNames, obj["cities"][i]["name"]];
+    options += '<option value="' + cityNames[i] + '" />';
+    i++;
+  }
+  /*console.log(cityNames);*/
+  document.getElementById("citieslist")!.innerHTML = options;
 }
+foo();
+
+/* cities end */
